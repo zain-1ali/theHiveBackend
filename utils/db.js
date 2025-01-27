ed65b9f51cdd1992ac1abd802a16534783ee17de
Zain Ali <zain.ali@724.one>
2025-01-27 15:27:38 +0500

    test

--- utils/db.js
+++ utils/db.js
@@ -9,9 +9,9 @@ exports.connectDb = async () => {
       .then(() => {
         console.log("db is conncted");
       })
-      .catch((error) => console.log(error));
+      .catch((error) => console.log(error, "some error in inner catch"));
   } catch (error) {
-    console.log(error);
+    console.log(error, "some error in outer catch");
     //  "mongodb+srv://zain114567:50I3VPy4fzcsA9t9@myprojects.ztxj7e9.mongodb.net/theHive?retryWrites=true&w=majority"
   }
 };
