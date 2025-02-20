11e62d71c80bf124e1faab34df02422412338cb4
Zain Ali <150250778+React724@users.noreply.github.com>
2025-02-20 16:11:11 +0500

    test

--- services/telegramService.js
+++ services/telegramService.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const bot = new TelegramBot(BOT_TOKEN, { polling: true });
+const bot = new TelegramBot(BOT_TOKEN);
 
 module.exports = { bot };
