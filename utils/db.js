1238024e602abbb07b9d7da35cd37536bb83169c
Zain Ali <150250778+React724@users.noreply.github.com>
2025-02-20 16:31:01 +0500

    db changed

--- utils/db.js
+++ utils/db.js
@@ -4,7 +4,8 @@ exports.connectDb = async () => {
   try {
     await mongoose
       .connect(
-        "mongodb+srv://admin:obxHIrvo9DfU7Yix@cluster0.6el2dvk.mongodb.net/theHive?retryWrites=true&w=majority"
+        // "mongodb+srv://admin:obxHIrvo9DfU7Yix@cluster0.6el2dvk.mongodb.net/theHive?retryWrites=true&w=majority"
+        "mongodb+srv://zain114567:50I3VPy4fzcsA9t9@myprojects.ztxj7e9.mongodb.net/theHive?retryWrites=true&w=majority"
         // {
         //   serverSelectionTimeoutMS: 10000, // 10 seconds
         //   socketTimeoutMS: 45000, // 45 seconds
